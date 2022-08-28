94cb6a680e64b0905c5fcec5cd02ad1855b6e416
Cesar <cordoba940408@gmail.com>
2022-08-27 23:01:48 -0500

    Formula

---
+++ Prueba.js
@@ -0,0 +1,6 @@
+function fuction1()  {
+    let a = prompt ("Digite número 1");
+    let b = prompt ("Digite número 2"); 
+    let c = parseInt(a) + parseInt(b);
+    document.getElementById("prueba").innerHTML = "The value of c is: " + c;
+}
